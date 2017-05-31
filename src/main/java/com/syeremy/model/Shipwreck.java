4c6c47dffa58c6fe60358b1e2dcaeea9a2e90c5e
syeremy <syeremy@gmail.com>
2017-05-30 20:37:01 -0500

    Enabling Lombok : removing verbose setters and getters from class! :D :D :D

--- src/main/java/com/syeremy/model/Shipwreck.java
+++ src/main/java/com/syeremy/model/Shipwreck.java
@@ -1,9 +1,14 @@
 package com.syeremy.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by syeremy on 5/30/17.
  */
 
+@Setter
+@Getter
 public class Shipwreck {
     Long id;
     String name;
@@ -26,68 +31,68 @@ public class Shipwreck {
         this.longitude = longitude;
         this.yearDiscovered = yearDiscovered;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCondition() {
-        return condition;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
-
-    public Integer getDepth() {
-        return depth;
-    }
-
-    public void setDepth(Integer depth) {
-        this.depth = depth;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Integer getYearDiscovered() {
-        return yearDiscovered;
-    }
-
-    public void setYearDiscovered(Integer yearDiscovered) {
-        this.yearDiscovered = yearDiscovered;
-    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getCondition() {
+//        return condition;
+//    }
+//
+//    public void setCondition(String condition) {
+//        this.condition = condition;
+//    }
+//
+//    public Integer getDepth() {
+//        return depth;
+//    }
+//
+//    public void setDepth(Integer depth) {
+//        this.depth = depth;
+//    }
+//
+//    public Double getLatitude() {
+//        return latitude;
+//    }
+//
+//    public void setLatitude(Double latitude) {
+//        this.latitude = latitude;
+//    }
+//
+//    public Double getLongitude() {
+//        return longitude;
+//    }
+//
+//    public void setLongitude(Double longitude) {
+//        this.longitude = longitude;
+//    }
+//
+//    public Integer getYearDiscovered() {
+//        return yearDiscovered;
+//    }
+//
+//    public void setYearDiscovered(Integer yearDiscovered) {
+//        this.yearDiscovered = yearDiscovered;
+//    }
 }

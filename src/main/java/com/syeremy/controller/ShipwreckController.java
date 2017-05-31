aa76be5a797d2ba525006fdc910125aa70a3e794
syeremy <syeremy@gmail.com>
2017-05-30 23:50:03 -0500

    USing ShipwrechRepository instead of ShipwreckStub

--- src/main/java/com/syeremy/controller/ShipwreckController.java
+++ src/main/java/com/syeremy/controller/ShipwreckController.java
@@ -1,6 +1,9 @@
 package com.syeremy.controller;
 
 import com.syeremy.model.Shipwreck;
+import com.syeremy.repository.ShipwreckRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,34 +16,41 @@ import java.util.List;
 @RequestMapping(value = "api/v1")
 public class ShipwreckController {
 
+    @Autowired
+    private ShipwreckRepository shipwreckRepository;
 
     @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
     public List<Shipwreck> list()
     {
-        return  ShipwreckStub.list();
+        return  shipwreckRepository.findAll();
     }
 
     @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
     public Shipwreck create(@RequestBody Shipwreck shipwreck)
     {
-        return  ShipwreckStub.create(shipwreck);
+        return  shipwreckRepository.saveAndFlush(shipwreck);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
     public Shipwreck get(@PathVariable long id)
     {
-        return  ShipwreckStub.get(id);
+        return  shipwreckRepository.findOne(id);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
     public Shipwreck update(@PathVariable long id, @RequestBody Shipwreck shipwreck)
     {
-        return  ShipwreckStub.update(id, shipwreck);
+        Shipwreck shipwreckCurrent = shipwreckRepository.findOne(id);
+        BeanUtils.copyProperties(shipwreck, shipwreckCurrent);
+        return  shipwreckRepository.saveAndFlush(shipwreckCurrent);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
     public Shipwreck delete(@PathVariable long id)
     {
-        return  ShipwreckStub.delete(id);
+        Shipwreck shipwreckCurrent = shipwreckRepository.findOne(id);
+        shipwreckRepository.delete(shipwreckCurrent);
+
+        return shipwreckCurrent;
     }
 }
